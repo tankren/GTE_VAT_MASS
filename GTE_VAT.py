2a98b939df204c2bcd0d74e6f49eac06ca3cfd8e
tankren <9392749+tankren@users.noreply.github.com>
2022-08-05 11:22:03 +0800

    use local Edge webdriver

--- GTE_VAT.py
+++ GTE_VAT.py
@@ -6,18 +6,17 @@ Created on Thu Aug  4 12:42:37 2022
 
 Tool to mass input the VAT on GTE Tax Recon website
 CMD:
-chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\selenum\ChromeProfile"
+chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\selenum\Profile"
 """
 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome  import ChromeDriverManager
-
-from requests import get
+#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service as EdgeService
+#from webdriver_manager.microsoft import EdgeChromiumDriverManager
+#from requests import get
 import pandas as pd
 import os
 import time
@@ -27,15 +26,18 @@ from tkinter.filedialog import askopenfilename
 os.popen("chcp 936")
 
 opt = Options()
-#opt.add_experimental_option("debuggerAddress", "localhost:9222")
-opt.add_argument("--remote-debugging-port=9222")
-opt.add_argument('user-data-dir=C:\\selenium\\ChromeProfile')
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=opt)
+opt.add_experimental_option("debuggerAddress", "localhost:9222")
+driver_path = EdgeService(r'./msedgedriver.exe')
+# opt.add_argument("--remote-debugging-port=9222")
+# opt.add_argument('user-data-dir=C:\\selenium\\Profile')
+#driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=opt)
+driver = webdriver.Edge(service=driver_path, options=opt)
 print(driver)
 
-driver.set_window_size(1024, 800)
+
 driver.get(url='https://www.xd.com/users/register/?')
 driver.implicitly_wait(10)
+
 
 def autofill(fphm, net, vat):
     driver.find_element(By.NAME, 'username').send_keys(fphm)
@@ -43,11 +45,12 @@ def autofill(fphm, net, vat):
     driver.find_element(By.NAME, 'confirm').send_keys('12345AAAAA!')
     driver.find_element(By.NAME, 'realname').send_keys(net)
     driver.find_element(By.NAME, 'realid').send_keys('110101199001013195')
-    driver.find_element(By.NAME, 'email').send_keys(vat,'@123.com')
+    driver.find_element(By.NAME, 'email').send_keys(vat, '@123.com')
     driver.find_element(By.NAME, 'mobile').send_keys('13800138000')
     driver.find_element(By.NAME, 'agreement').click()
     time.sleep(2)
     #driver.find_element(By.CLASS_NAME, 'geetest_radar_tip').click()
+
 
 Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
 
@@ -57,14 +60,13 @@ csvfile = askopenfilename(
 
 if not csvfile == '':
     df = pd.read_csv(csvfile, dtype=str, header=0)
-    
+
     col_list = df.values.tolist()
     row = 0
-    fail=[]
+    fail = []
     for item in col_list:
-        try: 
-            autofill(col_list[row][0], col_list[row][1],col_list[row][2])
+        try:
+            autofill(col_list[row][0], col_list[row][1], col_list[row][2])
             row = row + 1
         except:
             fail.append(col_list[row])
-        

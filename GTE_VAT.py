77ab08682a41b266257325969bd5ddfd59d82101
tankren <tankrenlive@gmail.com>
2022-08-09 14:57:39 +0800

    workable version

--- GTE_VAT.py
+++ GTE_VAT.py
@@ -22,84 +22,116 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome  import ChromeDriverManager
-from webdriver_manager.chrome  import ChromeDriverManager
 import pandas as pd
 import os
+import time
 from tkinter import Tk 
 from tkinter.filedialog import askopenfilename
+from tkinter import simpledialog
 
 os.popen("chcp 936")
-#year = input("请输入发票年份(YYYY):")
-#month = input("请输入发票月份(M):")
-year = ('2022')
-month = ('7')
 
-opt = Options()
-opt.add_experimental_option("debuggerAddress", "localhost:9222")
-"""
-opt.add_argument("--remote-debugging-port=9222")
-opt.add_argument('user-data-dir=C:\\selenium\\ChromeProfile')
-driver_path = ChromeService(r'./chromedriver.exe')
-driver = webdriver.Chrome(service=driver_path, options=opt)
-"""
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=opt)
-driver.set_window_size('1920', '1080')
-print(driver)
-"""
-driver.get(url='https://www.xd.com/users/register/?')
-driver.get(url='http://192.168.10.47:8080/glaf/loginApp.do')
-driver.implicitly_wait(5)
-driver.find_element(By.NAME, "x").click()
-driver.find_element(By.NAME, "x").clear()
-driver.find_element(By.NAME, "x").send_keys('3334')
-driver.find_element(By.NAME, "y1").click()
-driver.find_element(By.NAME, "y1").clear()
-driver.find_element(By.NAME, "y1").send_keys('KLnA67LW')
-driver.find_element(By.XPATH, '//button[@onclick="doLogin()"]').click()
-driver.implicitly_wait(5)
-driver.find_element(By.XPATH, '/html/body/aside/nav/ul/li[4]/a').click()
-"""
-driver.implicitly_wait(1)
-##driver.find_element(By.XPATH, '/html/body/aside/nav/ul/li[4]/ul/li[1]/a').click()
-driver.implicitly_wait(1)
-##driver.get(url='http://192.168.10.47:8080/glaf/index.do')
-driver.implicitly_wait(1)
-driver.find_element(By.XPATH, '//span[@id="select2-iyear-container"]').click()
-driver.implicitly_wait(1)
-driver.find_element(By.XPATH, '//span[@id="select2-imonth-container"]').click()
-driver.implicitly_wait(1)
-driver.find_element(By.XPATH, '//button[@id="Search_Btn"]').click()
+def autofill(num1, fphm, net, vat):
+    num2 = num1 + 6
+    num3 = num1 + 7
+    path1 = (r'//input[@id="_easyui_textbox_input{}"]'.format(num1))
+    path2 = (r'//input[@id="_easyui_textbox_input{}"]'.format(num2))
+    path3 = (r'//input[@id="_easyui_textbox_input{}"]'.format(num3)) 
+    print (path1, path2, path3)
+    driver.find_element(By.XPATH, path1).click()
+    driver.find_element(By.XPATH, path1).send_keys(fphm)
+    driver.find_element(By.XPATH, path2).click()
+    driver.find_element(By.XPATH, path2).send_keys(net)
+    driver.find_element(By.XPATH, path3).click()
+    driver.find_element(By.XPATH, path3).send_keys(vat)
 
 
-"""
-def autofill(fphm, net, vat):
-    driver.find_element(By.NAME, 'username').send_keys(fphm)
-    driver.find_element(By.NAME,  'password').send_keys('12345AAAAA!')
-    driver.find_element(By.NAME, 'confirm').send_keys('12345AAAAA!')
-    driver.find_element(By.NAME, 'realname').send_keys(net)
-    driver.find_element(By.NAME, 'realid').send_keys('110101199001013195')
-    driver.find_element(By.NAME, 'email').send_keys(vat,'@123.com')
-    driver.find_element(By.NAME, 'mobile').send_keys('13800138000')
-    driver.find_element(By.NAME, 'agreement').click()
-    driver.implicitly_wait(2)
-    #driver.find_element(By.CLASS_NAME, 'geetest_radar_tip').click()
-
-Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+Tk().withdraw()
 
 csvfile = askopenfilename(
     filetypes=[('CSV文件', '.csv')], title='选择发票信息CSV文件'
 )
+print(csvfile)
+
+year = simpledialog.askstring(title="请输入", prompt="发票年份(YYYY):")
+month = simpledialog.askstring(title="请输入", prompt="发票月份(M):")
+                
+#year = input("请输入发票年份(YYYY):")
+#month = input("请输入发票月份(M):")
+#year = ('2022')
+#month = ('7')
+
+opt = Options()
+opt.add_experimental_option("debuggerAddress", "localhost:9222")
+
+##opt.add_argument("--remote-debugging-port=9222")
+##opt.add_argument('user-data-dir=C:\\selenium\\ChromeProfile')
+"""
+driver_path = ChromeService(r'./chromedriver.exe')
+driver = webdriver.Chrome(service=driver_path, options=opt)
+"""
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=opt)
+driver.set_window_size('1920', '1080')
+print(driver)
 
 if not csvfile == '':
     df = pd.read_csv(csvfile, dtype=str, header=0)
     
     col_list = df.values.tolist()
     row = 0
+    num1 = 1
+    print(col_list[row])
     fail=[]
+
+    driver.get(url='http://192.168.10.47:8080/glaf/loginApp.do')
+    time.sleep(2)
+    driver.find_element(By.NAME, "x").click()
+    driver.find_element(By.NAME, "x").clear()
+    driver.find_element(By.NAME, "x").send_keys('3334')
+    driver.find_element(By.NAME, "y1").click()
+    driver.find_element(By.NAME, "y1").clear()
+    driver.find_element(By.NAME, "y1").send_keys('KLnA67LW')
+    driver.find_element(By.XPATH, '//button[@onclick="doLogin()"]').click()
+    time.sleep(2)
+    driver.get(url='http://192.168.10.47:8080/glaf/apps/bill.do?flag=billConfirm')
+    ##driver.find_element(By.XPATH, '/html/body/aside/nav/ul/li[4]/a').click()
+
+    ##driver.implicitly_wait(1)
+    ##driver.find_element(By.XPATH, '/html/body/aside/nav/ul/li[4]/ul/li[1]/a').click()
+
+    driver.find_element(By.XPATH, '//span[@id="select2-iyear-container"]').click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//input[@class="select2-search__field"]').send_keys(year)
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//li[@class="select2-results__option select2-results__option--highlighted"]').click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//span[@id="select2-imonth-container"]').click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//input[@class="select2-search__field"]').send_keys(month)
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//li[@class="select2-results__option select2-results__option--highlighted"]').click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//button[@id="Search_Btn"]').click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//input[@name="ck"]').click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//button[@onclick="javascript:invoice();"]').click()
+    time.sleep(3)
+    iframe = driver.find_element(By.XPATH, '//iframe[@id="layui-layer-iframe1"]')
+    driver.switch_to.frame("layui-layer-iframe1")
+    
     for item in col_list:
         try: 
-            autofill(col_list[row][0], col_list[row][1],col_list[row][2])
+            driver.find_element(By.XPATH, '//button[@id="Add_Btn"]').click()
+            print(num1, col_list[row][0], col_list[row][1],col_list[row][2])
+            autofill(num1, col_list[row][0], col_list[row][1],col_list[row][2])
+            num1 = num1 + 8
             row = row + 1
         except:
-            fail.append(col_list[row])
-"""
+            fail.append(col_list[row],'录入失败！')
+            print(fail)
+    #driver.find_element(By.XPATH, '//button[@id="Save_Btn"]).click()
+
+else:
+    print('请选择csv文件！')
+    driver.quit()
